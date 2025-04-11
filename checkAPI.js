9211adaab0028f1027d6497ac6ee78beba0eadd9
Jack Sparrow <133944655+LinuxDil@users.noreply.github.com>
2025-04-12 02:14:06 +0700

    Update checkAPI.js

--- checkAPI.js
+++ checkAPI.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const { log } = require("./utils"); // Menyesuaikan path sesuai kebutuhan
 const settings = require("./config/config");
 
-const urlChecking = "https://pro-api.animix.tech";
+const urlChecking = "https://raw.githubusercontent.com/LinuxDil/APIs-checking/refs/heads/main/endpoints.json";
 
 async function checkBaseUrl() {
   console.log("Memeriksa API...".blue);
